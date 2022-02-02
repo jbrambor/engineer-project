77d19718cd4cd75551383bf9b5077344503cd42f
rluczak <rluczak@edu.cdv.pl>
2022-02-02 22:24:36 +0100

    TAEPC-27 Configure Cypress & added example test

--- frontend/cypress/integration/example.spec.js
+++ frontend/cypress/integration/example.spec.js
@@ -1,5 +1,7 @@
 describe('The Home Page', () => {
     it('successfully loads', () => {
-        cy.visit('/') // change URL to match your dev URL
+        cy.visit('/')
+
+        cy.findByText("Learn React").should("be.visible");
     })
 })
