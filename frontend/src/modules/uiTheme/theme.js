6f9666e46bf0df40304c36cb1c43f6513e81ecb3
rluczak <rluczak@edu.cdv.pl>
2022-02-09 16:30:48 +0100

    TAEPC-31 added secondary colors to theme and comented neutrals colors

--- frontend/src/modules/uiTheme/theme.js
+++ frontend/src/modules/uiTheme/theme.js
@@ -7,17 +7,24 @@ export const theme = {
         primary80: "#B3D7FB", // Border
         primary100: "#F5FAFF", // Surface
 
+        // Secondary
+        secondary20: "#723C06", // Pressed
+        secondary40: "#C96909", // Hover
+        secondary60: "#E4780C", // Main
+        secondary80: "#F1B87F", // Border
+        secondary100: "#FBEBDA", // Surface
+
         // Neutrals
-        neutral00: "#041424",
-        neutral10: "#384756",
-        neutral20: "#445869",
-        neutral30: "#73808C",
-        neutral40: "#ACB3B9",
-        neutral50: "#C6CCD2",
-        neutral60: "#D6D9DC",
-        neutral70: "#EEF0F1",
-        neutral80: "#F9FAFA",
-        neutral90: "#FFFFFF",
+        neutral00: "#041424", // Headings, active text
+        neutral10: "#384756", // Body text, inactive text
+        neutral20: "#445869", // Caption text, secondary text
+        neutral30: "#73808C", // Placeholder text
+        neutral40: "#ACB3B9", // Disabled text and icon
+        neutral50: "#C6CCD2", // Borders
+        neutral60: "#D6D9DC", // Item background selected, dividers
+        neutral70: "#EEF0F1", // Item background pressed
+        neutral80: "#F9FAFA", // Item background hover, secondary background
+        neutral90: "#FFFFFF", // White
 
         // Warning
         warning20: "#6E4E0D", // Pressed
