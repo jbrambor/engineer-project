2aae731f819069dbcc09b0eed3deaf1c3111e69c
Radosław Łuczak <luczak.radoslaw1@gmail.com>
2022-03-21 22:05:20 +0100

    TAEPC-32 improve database and app configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from "@nestjs/config";
 
-import * as Joi from "@hapi/joi";
+import { configEnv } from './config/configuration';
 
-import { DatabaseModule } from "./modules/database/database.module";
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [
-      ConfigModule.forRoot({
-        validationSchema: Joi.object({
-          POSTGRES_HOST: Joi.string().required(),
-          POSTGRES_PORT: Joi.number().required(),
-          POSTGRES_USER: Joi.string().required(),
-          POSTGRES_PASSWORD: Joi.string().required(),
-          POSTGRES_DB: Joi.string().required(),
-          PORT: Joi.number(),
-        }),
-      }),
-      DatabaseModule,
-  ],
+  imports: [configEnv(), DatabaseModule],
   controllers: [],
   providers: [],
 })
